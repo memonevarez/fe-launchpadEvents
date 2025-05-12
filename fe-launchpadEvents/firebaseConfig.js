b7a8f0c6517e8a6609923d825ad2d744fc3c2ddb
César Guillermo Nevárez <memonevarez@hotmail.com>
2025-05-12 17:10:36 +0100

    firebase fix

--- fe-launchpadEvents/firebaseConfig.js
+++ fe-launchpadEvents/firebaseConfig.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBWcgscbusdiHHv4GHjlz9E57bleFjZT8",
-  authDomain: "launchpadevents-2864d.firebaseapp.com",
-  projectId: "launchpadevents-2864d",
-  storageBucket: "launchpadevents-2864d.firebasestorage.app",
-  messagingSenderId: "147010183960",
-  appId: "1:147010183960:web:97b75906b9b8d66e21746d",
-  measurementId: "G-WFCERDZCNQ",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
